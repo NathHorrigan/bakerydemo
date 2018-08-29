e749b349951b671673049053ac4d68388d418d6b
Chris Lawton <chris.lawton@torchbox.com>
2018-08-29 12:17:05 +0100

    make blog template use hero component

--- bakerydemo/static/frontend/src/components/hero/index.js
+++ bakerydemo/static/frontend/src/components/hero/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './hero.module.scss'
 
-export default ({ title, image, tag }) => (
-  <div className={styles.hero} style={{backgroundImage: `url(${image})`}}>
+export default ({ title, image, subtitle, tag }) => (
+  <div className={styles.hero} style={{ backgroundImage: `url(${image})` }}>
     <div className={styles.container}>
       <h1 className={styles.heroTitle}>{title}</h1>
+
+      {subtitle ? (
+        <p className={styles.heroSubtitle}>{subtitle}</p>
+      ) : (
+          null
+        )}
+
     </div>
   </div>
 )
