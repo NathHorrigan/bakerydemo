ece424b4370d518384b468ccb4c39f00902c1898
Chris Lawton <chris.lawton@torchbox.com>
2018-08-29 14:46:10 +0100

    add home page hero

--- bakerydemo/static/frontend/src/components/hero/index.js
+++ bakerydemo/static/frontend/src/components/hero/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import Link from 'gatsby-link'
+
 import styles from './hero.module.scss'
 
-export default ({ title, image, subtitle, tag }) => (
-  <div className={styles.hero} style={{ backgroundImage: `url(${image})` }}>
+export default ({ title, image, subtitle, modifier, ctaText, ctaLink, tag }) => (
+  <div className={[styles.hero, modifier].join(' ')} style={{ backgroundImage: `url(${image})` }}>
     <div className={styles.container}>
       <h1 className={styles.heroTitle}>{title}</h1>
 
       {subtitle ? (
         <p className={styles.heroSubtitle}>{subtitle}</p>
+      ) : (
+          null
+        )}
+
+      {ctaLink ? (
+        <Link to={ctaLink} className={styles.heroCta}>{ctaText}</Link>
       ) : (
           null
         )}
--- bakerydemo/static/frontend/src/pages/index.js
+++ bakerydemo/static/frontend/src/pages/index.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React from 'react';
+
+import Hero from '@components/hero'
 import { StreamFieldBlock } from '../fragments'
 
-export default () => {
+import { getMediaUrl } from '@util/urls'
+
+import styles from './home.module.scss'
+
+export default ({ data }) => {
+    const homeData = data.allPage.edges;
+    console.log(homeData);
+
     return (
-      <div>
-        Welcome to the Wagtail Bakery!
-      </div>
+        <div>
+            {homeData.map(({ node }) => {
+                return (
+                    <div key={node.id}>
+                        <Hero
+                            image={getMediaUrl(node.image.file.original)}
+                            title={node.title}
+                            subtitle={node.heroText}
+                            modifier={styles.homeHero}
+                            ctaText={node.heroCta}
+                            ctaLink={node.heroCtaLink.urlPath}
+                        />
+                    </div>
+                )
+            })}
+        </div>
     )
 }
+
+export const query = graphql`
+    query HomePageQuery {
+        allPage(filter: { type: { eq: "HomePage" } }) {
+            edges {
+                node {
+                    id
+                    title
+                    heroText
+                    heroCta
+                    heroCtaLink {
+                        urlPath
+                    }
+                    image {
+                        file {
+                            original
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
