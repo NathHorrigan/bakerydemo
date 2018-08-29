f8df9131280c4e39d6fe3eb24442e9c08a596f18
Chris Lawton <chris.lawton@torchbox.com>
2018-08-29 10:10:22 +0100

    lint

--- bakerydemo/static/frontend/src/pages/blog/index.js
+++ bakerydemo/static/frontend/src/pages/blog/index.js
@@ -5,15 +5,15 @@ import { getMediaUrl, parseDate } from '@util/urls'
 
 import styles from './blog.module.scss'
 
-export default ({data}) => {
+export default ({ data }) => {
     const blogs = data.allPage.edges;
     return (
         <div className={styles.container}>
             <div className={styles.blogContainer}>
-                {blogs.map(({node}) => {
+                {blogs.map(({ node }) => {
                     return (
                         <Link to={node.slug} key={node.id} className={styles.blog}>
-                            <img className={styles.blogImage} src={getMediaUrl(node.image.file.thumbnail)}/>
+                            <img className={styles.blogImage} src={getMediaUrl(node.image.file.thumbnail)} />
                             <div className={styles.blogText}>
                                 <h2 className={styles.blogTitle}>{node.title}</h2>
                                 <p className={styles.blogIntroduction}>{node.introduction}</p>
