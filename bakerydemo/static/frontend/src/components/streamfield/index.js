6ec128a01c30a4f6a286ea08e123cd2549fe3a96
Chris Lawton <chris.lawton@torchbox.com>
2018-08-30 10:45:47 +0100

    add keys to streamfield blocks and link styling for paragraph blocks

--- bakerydemo/static/frontend/src/components/streamfield/index.js
+++ bakerydemo/static/frontend/src/components/streamfield/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 import { ParagraphBlock, HeadingBlock } from './blocks'
 
-export default ({blocks}) => blocks.map(block => {
-  console.log(block)
+export default ({ blocks }) => blocks.map(block => {
   switch (block.type) {
 
     case 'heading_block':
-      return <HeadingBlock content={block.value['heading_text']} size={block.value.size}/>
+      return <HeadingBlock key={block.value} content={block.value['heading_text']} size={block.value.size} />
 
     case 'paragraph_block':
-      return <ParagraphBlock content={block.value.content}/>
+      return <ParagraphBlock key={block.value} content={block.value.content} />
 
     default:
       return <h3>Unknown Block Type: {block.type}</h3>
